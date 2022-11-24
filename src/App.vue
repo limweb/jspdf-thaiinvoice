c0b9dd31145070fed17dfe971525b99d7dd670bf
Thongchai Lim (样生海) <lim.thongchai@gmail.com>
2022-11-24 13:33:47 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-green-500 text-red" style="background-color: coral">
+    Header
+  </div>
+  <Invoice />
+  <div class="bg-red-200">Footer</div>
+</template>
+
+<script>
+import Invoice from "./components/Invoice.vue";
+export default {
+  name: "App",
+  components: {
+    Invoice,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.sj
@@ -0,0 +1,21 @@
+const UnoCSS = require('@unocss/webpack').default
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      UnoCSS({}),
+    ],
+  },
+  chainWebpack(config) {
+    config.module.rule('vue').uses.delete('cache-loader')
+    config.module.rule('tsx').uses.delete('cache-loader')
+    config.merge({
+      cache: false,
+    })
+  },
+  css: {
+    extract: {
+      filename: '[name].[hash:9].css',
+    },
+  },
+}
